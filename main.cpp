818e61eee66ffeea8df8478a85c819d99327b3cf
yonde927 <svyuta1202@gmail.com>
2022-01-09 23:33:50 +0900

    ssh

--- main.cpp
+++ main.cpp
@@ -6,14 +6,20 @@ private:
 	ssh_session ssh;
 public:
 	sftp(){
-		
+		ssh = ssh_new();
+		ssh_options_set(ssh, SSH_OPTIONS_HOST, "localhost");
+		if(ssh_connect(ssh)!=SSH_OK){
+			std::cerr << "ssh_connect failed: " << ssh_get_error(ssh) << std::endl;
+		}
+		std::cout << "connect success" << std::endl;
+	}
+	~sftp(){
+		ssh_disconnect(ssh);
+		ssh_free(ssh);
 	}
 };
 
-
-
 int main(){
-	std::cout << "Hello" << std::endl;
-	std::cout << "Yuta" << std::endl;
+	sftp* p_connector = new sftp;
 	return 0;
 }
